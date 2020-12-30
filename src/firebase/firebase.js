ebc9c3387e2ad310010e36a1ba3273abac12b009
Pratish7 <pratishbajpai7@gmail.com>
2020-12-30 23:34:41 +0530

    added firebaseConfig

---
+++ src/firebase/firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apikey: '',
+    databseURL: '',
+    projectID: '',
+    appId: ''
+};
+
+export default firebase.initializeApp(firebaseConfig);
