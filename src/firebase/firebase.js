06fe3e02744d4c3c2c675ec1dbb8982e6510a815
Pratish7 <pratishbajpai7@gmail.com>
2021-01-02 12:50:11 +0530

    added firebase details

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,10 +1,11 @@
 import firebase from 'firebase';
+import data from './details'
 
 const firebaseConfig = {
-    apikey: '',
-    databseURL: '',
-    projectID: '',
-    appId: ''
+    apikey: data.apikey,
+    databseURL: data.databseURL,
+    projectID: data.projectID,
+    appId: data.appId
 };
 
 export default firebase.initializeApp(firebaseConfig);
