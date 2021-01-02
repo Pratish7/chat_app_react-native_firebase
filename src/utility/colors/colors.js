1895c59c06702f00ef4a5cf19fb14c0e2bfc0872
Pratish7 <pratishbajpai7@gmail.com>
2021-01-02 18:03:01 +0530

    added colors

--- src/utility/colors/colors.js
+++ src/utility/colors/colors.js
@@ -1,7 +1,19 @@
 const colors = {
-    WARNING: 'rgb(236,76,60)',
-    BLACK: 'rgb(0,0,0)',
-    DARK_GRAY: 'rgb(46,46,46)',
-    WHITE: 'rgb(255,255,255)'
+    SECONDARY: "rgb(32, 79, 104)",
+    WARNINGNERROR: "rgb(231, 76, 60)",
+    DANGER: "rgb(255,0,0)",
+    GREY: "rgb(219, 221, 222)",
+    WHITE: "rgb(255, 255, 255)",
+    BLACK: "rgb(0, 0, 0)",
+    SEMI_TRANSPARENT: "rgba(0,0,0,0.5)",
+    RED: "rgba(255, 0, 0, 1)",
+    SILVER: "rgb(192,192,192)",
+    DARK_GRAY: "rgb(46, 46, 46)",
+    DARK_LIME_GREEN: "rgba(0,128,0,0.2)",
+    TRANSPARENT: "transparent",
+    GRADIENT_PRIMARY: ["rgb(0, 169, 244)", "rgb(0, 169, 244)"],
+    LIGHT_GREEN: "#99BA93",
+    GREEN: "rgb(0,120,0)",
+    BORDER_LIGHT_GREYCOLOR: "#EFE9EB",
 }
 export default colors;
