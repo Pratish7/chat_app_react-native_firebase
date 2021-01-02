3ce1a849405005a373e42ee80d8ac19bd9e5cbcb
Pratish7 <pratishbajpai7@gmail.com>
2021-01-02 18:40:29 +0530

    created login screen

--- src/container/login/login.js
+++ src/container/login/login.js
@@ -1,10 +1,62 @@
-import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+import colors from '../../utility/colors/colors';
+import globalStyle from '../../utility/style/globalStyle';
+import Logo from '../../component/logo/logo'
+import InputField from '../../component/input/input';
+import Button from '../../component/button/button';
 
 const login = ({ navigation }) => {
+    const [credentials, setCreadentials] = useState({
+        email: '',
+        password: ''
+    });
+
+    const { email, password } = credentials;
+
+    const onChangeHandeler = (name, value) => {
+        setCreadentials({
+            ...credentials,
+            [name]: value
+        });
+
+    };
+
+    const loginPressHandeler = () => {
+        if (!email) alert('Email can\'t be blank');
+        else if (!password) alert('Password can\'t be blank');
+        else alert(JSON.stringify(credentials));
+    }
+
+
+
     return (
-        <SafeAreaView>
-            <Text onPress={() => navigation.navigate('Signup')}>Login</Text>
+        <SafeAreaView style={[globalStyle.flex1, { backgroundColor: colors.BLACK }]}>
+            <View style={[globalStyle.containerCentered]} >
+                <Logo />
+            </View>
+            <View style={[globalStyle.flex2, globalStyle.sectionCentered]}>
+                <InputField
+                    placeholder='Email'
+                    value={email}
+                    onChangeText={(text) => onChangeHandeler('email', text)}
+                />
+                <InputField
+                    placeholder='Password'
+                    secureTextEntry={true}
+                    value={password}
+                    onChangeText={(text) => onChangeHandeler('password', text)}
+                />
+                <Button title='Login' onPress={() => loginPressHandeler()} />
+                <Text style={{
+                    fontSize: 20,
+                    fontWeight: 'bold',
+                    color: colors.LIGHT_GREEN
+                }}
+                    onPress={() => navigation.navigate('Signup')}>
+                    Sign Up
+                </Text>
+            </View>
         </SafeAreaView>
     );
 };
