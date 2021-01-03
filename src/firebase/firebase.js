537d2855b0afe567ebdc4ac12e0604de138c3d55
Pratish7 <pratishbajpai7@gmail.com>
2021-01-03 15:37:23 +0530

    minor tweak

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,11 +1,11 @@
-import firebase from 'firebase';
+import Firebase from 'firebase';
 import data from './details'
 
 const firebaseConfig = {
-    apikey: data.apikey,
-    databseURL: data.databseURL,
-    projectID: data.projectID,
-    appId: data.appId
+    apiKey: 'AIzaSyCpLDjAaMHV2_2pPJenxYWfUaF6NbRXLXE',
+    databaseURL: 'https://firechat-e1629-default-rtdb.firebaseio.com/',
+    projectId: 'firechat-e1629',
+    appId: '1:1032657147261:android:7d5a0e7bb2ae571c76a814'
 };
 
-export default firebase.initializeApp(firebaseConfig);
+export default Firebase.initializeApp(firebaseConfig);
